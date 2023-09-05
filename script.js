b4c03ed657bdef08f8e1cb2016522c4e3f9147a7
RiP-7 <allaccwork7@gmail.com>
2023-09-05 20:27:38 +0530

    idk man

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quotes = [
+const quotes= [
     "If you want to talk, please get out of the class and talk.", 
     "Don't try to act oversmart with me.", 
     "Talk loudly! You did not have breakfast or what?",
@@ -27,25 +27,58 @@ const quotes = [
 
  
    
-const quoteElement = document.getElementById("quote");
-const newQuoteButton = document.getElementById("new-quote");
+    const backgrounds = [
+      "https://images.hindustantimes.com/img/2022/01/20/1600x900/a0aaf782-7a22-11ec-92d4-e30affc03428_1642705111219.jpg",
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/2B1A/production/_124343011_gettyimages-144104082.jpg",
+      "https://inc42.com/wp-content/uploads/2019/09/teacher-featured.jpg",
+      "https://img.freepik.com/premium-photo/blank-chalkboard-wall-classroom-3d-render_124437-1959.jpg",
+      "https://www.heraldgoa.in/uploads/news/big_179675_school.png",
 
-function getRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  return quotes[randomIndex];
-}
-
-function changeBackgroundColor() {
-  const colors = ["#FFF3DA", "#ADC4CE6", "#116A7B", "#A4BC92", "#CEEDC7","#CYEDC7" ];
-  const randomColorIndex = Math.floor(Math.random() * colors.length);
-  document.body.style.backgroundColor = colors[randomColorIndex];
-}
-
-newQuoteButton.addEventListener(click, () => {
-  const randomQuote = getRandomQuote();
-  quoteElement.textContent = randomQuote;
-  changeBackgroundColor();
-});
-
-// Initial quote and background color change
-newQuoteButton.click();
+      
+    
+    ];
+    
+    function getRandomQuote() {
+      return quotes[Math.floor(Math.random() * quotes.length)];
+    }
+    
+    function getRandomColor() {
+      const colors = [
+        "#740001", "#1a472a", "#0e1a40", "#ecb939",
+        "#ae0001", "#2a623d", "#222f5b", "#f0c75e",
+        "#eeba30", "#5d5d5d", "#726255",
+        "#d3a625", "#946b2d", "#372e29"
+      ];
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
+    
+    function getRandomBackground() {
+      const randomIndex = getRandomIndex(backgrounds.length);
+      const imageUrl = `url(${backgrounds[randomIndex]})`;
+      document.body.style.backgroundImage = imageUrl;
+    }
+    
+    function getRandomIndex(arrayLength) {
+      return Math.floor(Math.random() * arrayLength);
+    }
+    
+    function displayQuote() {
+      const { text, author } = getRandomQuote();
+      const randomColor = getRandomColor();
+      
+      document.body.style.backgroundColor = randomColor; // Set background color
+      document.querySelector("button").style.backgroundColor = randomColor; // Set button background color
+      document.querySelector(".quote").textContent = text;
+      document.querySelector(".author").textContent = "- " + author;
+      document.querySelector(".quote").style.color = randomColor; // Set text color
+      document.querySelector(".author").style.color = randomColor;
+      document.querySelector("#top-left-text").style.color = randomColor; // Set author color
+      
+      getRandomBackground(); // Set the background image
+    }
+    
+    document.getElementById("new-quote").addEventListener("click", displayQuote);
+    
+    // Display a random quote and change colors when the page loads
+    displayQuote();
+    
