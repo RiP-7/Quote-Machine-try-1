2fcdd7c9b902ab9713a3e9daa1d9db828110e9f6
RiP-7 <allaccwork7@gmail.com>
2023-09-07 00:03:58 +0530

    FIXED YAY

--- script.js
+++ script.js
@@ -15,6 +15,16 @@ const quotes= [
   author: "Your teacher"},
   {text:"Be quiet! Is this a class or a fish market?",
   author: "Your teacher"},
+   {text:"I took the free period to teach my subject.",
+  author: "Your teacher"},
+  {text:"*Bangs the duster on the table*",
+  author: "Your teacher"},
+  {text:"Who did this? Tell me or no one goes home!",
+  author: "Your teacher"},
+  {text:"Are you here only to waste your parents' money?",
+  author: "Your teacher"},
+  {text:"I will not tolerate such behaviour in my class!",
+  author: "Your teacher"},
   {text:"We are so behind on the portion! We need to catch up like A division.",
   author: "Your teacher"},
   {text:"Give me your parents phone number.",
@@ -26,16 +36,6 @@ const quotes= [
   {text:"Tell me when you all have finished talking",
   author: "Your teacher"},
   {text:"Your body is here but your mind is wandering. Physically you are here but where are you mentally?",
-  author: "Your teacher"},
-  {text:"I took the free period to teach my subject.",
-  author: "Your teacher"},
-  {text:"*Bangs the duster on the table*",
-  author: "Your teacher"},
-  {text:"Who did this? Tell me or no one goes home!",
-  author: "Your teacher"},
-  {text:"Are you here only to waste your parents' money?",
-  author: "Your teacher"},
-  {text:"I will not tolerate such behaviour in my class!",
   author: "Your teacher"},
   {text:"You think we teachers are fools to teach you?",
   author: "Your teacher"},
@@ -56,10 +56,17 @@ const quotes= [
       "https://ichef.bbci.co.uk/news/976/cpsprodpb/2B1A/production/_124343011_gettyimages-144104082.jpg",
       "https://inc42.com/wp-content/uploads/2019/09/teacher-featured.jpg",
       "https://img.freepik.com/premium-photo/blank-chalkboard-wall-classroom-3d-render_124437-1959.jpg",
+      "https://i0.wp.com/mumbai7.com/wp-content/uploads/2016/04/dreamstime_s_193769319.jpg",
       "https://www.heraldgoa.in/uploads/news/big_179675_school.png",
+      "https://www.thestatesman.com/wp-content/uploads/2019/04/iStock-1072470136.jpg",
+      "https://www.shivalik.org/assets/ckfinder_old/userfiles/files/IMG_6042.JPG",
+      "https://static.toiimg.com/photo/msid-95263609/95263609.jpg",
+      "https://indianschoolraigarh.in/controlpanel/mwsimages/gallery10.jpg",
+      "https://cdn2.hubspot.net/hubfs/2174926/Global%20Learning/GIIS_QT_SportsDay_03-02-17-06-11-30.jpg",
+      "https://static.toiimg.com/photo/msid-96530918/96530918.jpg",
+      "https://jawaharcbse.in/wp-content/uploads/2023/02/lab-2048x1638.jpg",
+      "https://www.roksha.in/wp-content/uploads/2019/07/Auditorium_5-100.jpg"
 
-      
-    
     ];
     
     function getRandomQuote() {
@@ -69,7 +76,7 @@ const quotes= [
     function getRandomColor() {
       const colors = [
         "#740001", "#1a472a", "#0e1a40", "#ecb939",
-        "#ae0001", "#2a623d", "#222f5b", "#f0c75e",
+        "#8f569a", "#2a623d", "#222f5b", "#f0c75e",
         "#eeba30", "#5d5d5d", "#726255",
         "#d3a625", "#946b2d", "#372e29"
       ];
